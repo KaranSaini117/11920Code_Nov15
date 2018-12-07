1fc748ba8e2e397af36f9a8bd5fadf5d222ae21e
Karan Saini <karansaini117@gmail.com>
2018-12-07 15:51:43 -0800

    i would not read this smh
    yeee lmaooo sup future person reading in the future hope we have flying cards biachhhhh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LowerCraterSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LowerCraterSample.java
@@ -29,6 +29,8 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -53,7 +55,7 @@ import java.util.List;
  * is explained below.
  */
 @Autonomous(name = "LowerCraterSample", group = "11920")
-//@Disabled
+@Disabled
 public class LowerCraterSample extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
     private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
